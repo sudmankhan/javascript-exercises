da4f150437c9007e148c340e8cbdb85c454abe09
Sudman <sudmankhan1293@gmail.com>
2024-07-25 23:53:05 -0400

    completed up to excercise 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
+    const newArray =[];
+    for (let i = 0; i < array.length; i++) {
+        let valid = true;
+        for(let j = 1; j < arguments.length; j++) {
+            if (array[i] === arguments[j]) {
+                valid = false;
+            }
+        }
+        if (valid) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
